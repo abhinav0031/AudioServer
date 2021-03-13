55cb2dc4eea126fcd15daf249af094aed6ed2e12
abhinav0031 <abhinav16100@gmail.com>
2021-03-13 11:33:33 +0530

    fix more small bugs

--- app/server/routes/audio.py
+++ app/server/routes/audio.py
@@ -32,29 +32,31 @@ router = APIRouter()
 @router.post("/addaudio", response_description="Song data added into the database")
 async def add_song_data(audio: AudioSchema):
     audio = jsonable_encoder(audio)
-    if audio['type'] == 'song':
+    if audio['type'].lower() == 'song':
         new_song = await add_song(audio)
         return ResponseModel(new_song, "Audio added successfully.")
-    elif audio['type'] == 'podcast':
+    elif audio['type'].lower() == 'podcast':
         new_podcast = await add_podcast(audio)
         return ResponseModel(new_podcast, "Audio added successfully.")
+    elif audio['type'].lower() == 'audiobook':
+        new_audiobook = await add_audiobook(audio)
+        return ResponseModel(new_audiobook, "Audio added successfully.")
 
-    new_audiobook = await add_audiobook(audio)
-    return ResponseModel(new_audiobook, "Audio added successfully.")
+    return ErrorResponseModel("An error occurred.", 400, "fill audiotype from following : song,podcast or audiobook")
 
 
 @router.get("/{audioType}", response_description="audio retrieved")
 async def get_audio(audioType: str):
-    if audioType == 'song':
+    if audioType.lower() == 'song':
         audios = await retrieve_songs()
         if audios:
             return ResponseModel(audios, "audio data retrieved successfully")
 
-    elif audioType == 'podcast':
+    elif audioType.lower() == 'podcast':
         audios = await retrieve_podcasts()
         if audios:
             return ResponseModel(audios, "audio data retrieved successfully")
-    elif audioType == 'audiobook':
+    elif audioType.lower() == 'audiobook':
         audios = await retrieve_audiobooks()
         if audios:
             return ResponseModel(audios, "audio data retrieved successfully")
@@ -64,15 +66,15 @@ async def get_audio(audioType: str):
 
 @router.get("/{audioType}/{id}", response_description="audio retrieved")
 async def get_audiobyid(audioType: str, id: str):
-    if audioType == 'song':
+    if audioType.lower() == 'song':
         audio = await retrieve_song(id)
         if audio:
             return ResponseModel(audio, "audio data retrieved successfully")
-    elif audioType == 'podcast':
+    elif audioType.lower() == 'podcast':
         audio = await retrieve_podcast(id)
         if audio:
             return ResponseModel(audio, "audio data retrieved successfully")
-    elif audioType == 'audiobook':
+    elif audioType.lower() == 'audiobook':
         audio = await retrieve_audiobook(id)
         if audio:
             return ResponseModel(audio, "audio data retrieved successfully")
@@ -81,21 +83,21 @@ async def get_audiobyid(audioType: str, id: str):
 
 @router.delete("/{audioType}/{id}", response_description="audio retrieved")
 async def delete_audiobyid(audioType: str, id: str):
-    if audioType == 'song':
+    if audioType.lower() == 'song':
         deleted_audio = await delete_song(id)
         if deleted_audio:
             return ResponseModel(
                 "Audio with ID: {} removed".format(
                     id), "Audio deleted successfully"
             )
-    elif audioType == 'podcast':
+    elif audioType.lower() == 'podcast':
         deleted_audio = await delete_podcast(id)
         if deleted_audio:
             return ResponseModel(
                 "Audio with ID: {} removed".format(
                     id), "Audio deleted successfully"
             )
-    elif audioType == 'audiobook':
+    elif audioType.lower() == 'audiobook':
         deleted_audio = await delete_audiobook(id)
         if deleted_audio:
             return ResponseModel(
@@ -110,7 +112,7 @@ async def delete_audiobyid(audioType: str, id: str):
 @router.put("/{audioType}/{id}")
 async def update_audio_data(audioType: str, id: str, req: UpdateAudioModel = Body(...)):
 
-    if audioType == 'song':
+    if audioType.lower() == 'song':
         req = {k: v for k, v in req.dict().items() if v is not None}
         updated_audio = await update_song(id, req)
         if updated_audio:
@@ -118,7 +120,7 @@ async def update_audio_data(audioType: str, id: str, req: UpdateAudioModel = Bod
                 "Audio with ID: {} name update is successful".format(id),
                 "Audio name updated successfully",
             )
-    elif audioType == 'podcast':
+    elif audioType.lower() == 'podcast':
         req = {k: v for k, v in req.dict().items() if v is not None}
         updated_audio = await update_podcast(id, req)
         if updated_audio:
@@ -126,7 +128,7 @@ async def update_audio_data(audioType: str, id: str, req: UpdateAudioModel = Bod
                 "Audio with ID: {} name update is successful".format(id),
                 "Audio name updated successfully",
             )
-    elif audioType == 'audiobook':
+    elif audioType.lower() == 'audiobook':
         req = {k: v for k, v in req.dict().items() if v is not None}
         updated_audio = await update_audiobook(id, req)
         if updated_audio:
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@ How to start the app (all these commands were tested on windows 10)
 
 commands(all these commands should be executed in the project folder(i personaly have used visual studio code terminal for it)):
 
-1  run command-  pip install venv
-2  run command-  venv myenv           //myenv is name given to virtual environment you can use any name of your choice
+1  run command-  pip install virtualenv
+2  run command-  virtualenv myenv           //myenv is name given to virtual environment you can use any name of your choice
 3  now to activate venv use command-   source ./myenv/Scripts/activate
 4  now run command-   export PYTHONPATH=$PWD 
 5  now when your virtual environment is activated install dependencies using command-    pip install -r requirements.txt
@@ -13,12 +13,19 @@ your server should be running now on http://localhost:8000/
 
 as i have used fastapi you can go to http://localhost:8000/docs to interact with routes.
 
+
+
 breif summary on routes:
 
 [POST]
 /addaudio: in this route you first need to add type of audio and then fill the metadata details of that type,if you want to you 
             can remove other type's metadata fields whose type you didn't entered.
 
+note: use audioType one of these three: song,podcast or audiobook.
+
+
+audioType can be: song,podcast or audiobook
+please enter correctly otherwise error will occur.
 [GET]
 /{audioType}: this route will give you all the audios stored on database of the type you entered.
 /{audioType}/{id}: this route will give you audio with the given id and audioType.
